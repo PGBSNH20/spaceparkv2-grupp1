ab5b4789d13bbc1088249cd8012aeb68959921a5
Liam Björkman <liambjorkman00@gmail.com>
2021-04-30 11:11:36 +0200

    Added payments table & seed data

---
+++ Source/SpaceApi/Migrations/20210430091028_AddPayments.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SpaceApi.Migrations
+{
+    public partial class AddPayments : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Payments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Amount = table.Column<int>(type: "int", nullable: false),
+                    User = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    PayDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Payments", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Parkings",
+                columns: new[] { "Id", "Fee", "MaxLength", "Occupied", "ParkedBy", "ShipName" },
+                values: new object[,]
+                {
+                    { 1, 10, 50m, false, null, null },
+                    { 2, 50, 100m, false, null, null },
+                    { 3, 100, 200m, false, null, null },
+                    { 4, 1000, 2000m, false, null, null },
+                    { 5, 5, 15m, false, null, null }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Payments");
+
+            migrationBuilder.DeleteData(
+                table: "Parkings",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Parkings",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Parkings",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Parkings",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Parkings",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
