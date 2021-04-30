7d7e3f35bafa476d92d47fc41ede9eed0b799c47
Liam Björkman <liambjorkman00@gmail.com>
2021-04-30 11:21:33 +0200

    Console app

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var running = true;
+            while (running)
+            {
+                StandardMessages.StartMessage();
+                var selectedOption = Menu.ShowMenu("SpacePark Menu", new[]
+                {
+                    "Park Ship",
+                    "Leave SpacePark",
+                    "Show Receipts",
+                    "Exit Menu"
+                });
+                switch (selectedOption)
+                {
+                    case 0:
+                        //if (Occupation.AllParksOccupied()) break;
+                        ////var ship = starship.SelectShip();
+                        //if (ship == null) break;
+                        ////parking.Park(ship);
+                        break;
+                    case 1:
+                        //leave.LeavePark();
+                        break;
+                    case 2:
+                        //payment.Receipts();
+                        break;
+                    case 3:
+                        running = false;
+                        break;
+                }
+            }
+        }
+    }
+}
