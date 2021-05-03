b20f24a4aaba9d14f49d88640b76a7e933bc0a0b
Liam Björkman <liambjorkman00@gmail.com>
2021-05-03 13:37:35 +0200

    Comments

--- Logic/SpacePark/SpaceParkApi.cs
+++ Logic/SpacePark/SpaceParkApi.cs
@@ -10,22 +10,18 @@ namespace Logic.SpacePark
 {
     public class SpaceParkApi
     {
-        public static async Task<Parking> GetParking(int id)
+        public static async Task<Parking> GetParking(int id) // Return the specfied parking lot
         {
             var client = new RestClient("https://localhost:50886/api/");
             var request = new RestRequest($"Parkings/{id}", DataFormat.Json);
             var response = await client.GetAsync<Parking>(request);
             return response;
         }
-        public static async Task<List<Parking>> GetAllParkings()
+        public static async Task<List<Parking>> GetAllParkings() // Return a list of all parkings
         {
             var client = new RestClient("https://localhost:50886/api/");
             var request = new RestRequest("Parkings/", DataFormat.Json);
             var response = await client.GetAsync<List<Parking>>(request);
-
-            Console.WriteLine(response[0].ShipName);
-
-
             return response;
         }
 
