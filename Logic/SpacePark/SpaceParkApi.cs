bb86af8783965bc74bba440d34ddb0fbe3dedf9e
Liam Björkman <liambjorkman00@gmail.com>
2021-05-03 13:54:31 +0200

    Fixed API ports

--- Logic/SpacePark/SpaceParkApi.cs
+++ Logic/SpacePark/SpaceParkApi.cs
@@ -12,14 +12,14 @@ namespace Logic.SpacePark
     {
         public static async Task<Parking> GetParking(int id) // Return the specfied parking lot
         {
-            var client = new RestClient("https://localhost:50886/api/");
+            var client = new RestClient("https://localhost/api/");
             var request = new RestRequest($"Parkings/{id}", DataFormat.Json);
             var response = await client.GetAsync<Parking>(request);
             return response;
         }
         public static async Task<List<Parking>> GetAllParkings() // Return a list of all parkings
         {
-            var client = new RestClient("https://localhost:50886/api/");
+            var client = new RestClient("https://localhost/api/");
             var request = new RestRequest("Parkings/", DataFormat.Json);
             var response = await client.GetAsync<List<Parking>>(request);
             return response;
