44404496dade1add5d0e1ed28028c9446ddfc87c
Liam Björkman <liambjorkman00@gmail.com>
2021-05-05 15:28:58 +0200

    Added api key auth

--- Logic/SpacePark/SpaceParkApi.cs
+++ Logic/SpacePark/SpaceParkApi.cs
@@ -4,6 +4,7 @@ using System.Text.Json;
 using System.Threading.Tasks;
 using Logic.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using RestSharp;
 using SpaceApi.Models;
 
@@ -14,6 +15,7 @@ namespace Logic.SpacePark
         public async Task<Parking> GetParkingById(int id) // Return the specfied parking lot
         {
             var client = new RestClient("https://localhost/api/");
+            client.AddDefaultHeader("apikey", "secret1234");
             var request = new RestRequest($"parkings/{id}", DataFormat.Json);
             var response = await client.GetAsync<Parking>(request);
             return response;
@@ -21,6 +23,7 @@ namespace Logic.SpacePark
         public async Task<List<Parking>> GetParkingByPortName(string portName) // Return the specfied parking lot
         {
             var client = new RestClient("https://localhost/api/");
+            client.AddDefaultHeader("apikey", "secret1234");
             var request = new RestRequest($"parkings?portname={portName}", DataFormat.Json);
             var response = await client.GetAsync<List<Parking>>(request);
             return response;
@@ -28,6 +31,7 @@ namespace Logic.SpacePark
         public async Task<List<Parking>> GetAllParkings() // Return a list of all parkings
         {
             var client = new RestClient("https://localhost/api/");
+            client.AddDefaultHeader("apikey", "secret1234");
             var request = new RestRequest("parkings/", DataFormat.Json);
             var response = await client.GetAsync<List<Parking>>(request);
             return response;
@@ -36,6 +40,7 @@ namespace Logic.SpacePark
         public async Task<List<SpacePort>> GetAllSpacePorts()
         {
             var client = new RestClient("https://localhost/api/");
+            client.AddDefaultHeader("apikey", "secret1234");
             var request = new RestRequest("spaceports/", DataFormat.Json);
             var response = await client.GetAsync<List<SpacePort>>(request);
             return response;
@@ -43,6 +48,7 @@ namespace Logic.SpacePark
         public IRestResponse Park(Parking parking) // Update existing parking - Used for parking a ship, can also be used to remove a ship
         {
             var client = new RestClient("https://localhost/api/");
+            client.AddDefaultHeader("apikey", "secret1234");
             var request = new RestRequest("parkings/" + parking.Id, Method.PUT);
             request.AddJsonBody(parking);
             return client.Execute(request);
@@ -50,6 +56,7 @@ namespace Logic.SpacePark
         public IRestResponse AddSpacePort(SpacePort spacePort)
         {
             var client = new RestClient("https://localhost/api/");
+            client.AddDefaultHeader("apikey", "secret1234");
             var request = new RestRequest("spaceports/", Method.POST);
             request.AddJsonBody(spacePort);
             return client.Execute(request);
@@ -57,12 +64,14 @@ namespace Logic.SpacePark
         public IRestResponse RemoveSpacePort(SpacePort spacePort)
         {
             var client = new RestClient("https://localhost/api/");
+            client.AddDefaultHeader("apikey", "secret1234");
             var request = new RestRequest("spaceports/" + spacePort.Id, Method.DELETE);
             return client.Execute(request);
         }
         public IRestResponse AddParking(Parking parking)
         {
             var client = new RestClient("https://localhost/api/");
+            client.AddDefaultHeader("apikey", "secret1234");
             var request = new RestRequest("parkings/", Method.POST);
             request.AddJsonBody(parking);
             return client.Execute(request);
@@ -70,6 +79,7 @@ namespace Logic.SpacePark
         public IRestResponse RemoveParking(Parking parking)
         {
             var client = new RestClient("https://localhost/api/");
+            client.AddDefaultHeader("apikey", "secret1234");
             var request = new RestRequest("parkings/" + parking.Id, Method.DELETE);
             return client.Execute(request);
         }
